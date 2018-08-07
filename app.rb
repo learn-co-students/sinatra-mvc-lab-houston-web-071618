c617bfd160a61d178d3988fa84a5f79a8716a549
Sean Huang <hyprul@ucla.edu>
2018-08-07 15:19:21 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
+require_relative './models/piglatinizer.rb'
 
 class App < Sinatra::Base
+
+	get '/' do 
+		erb :user_input
+	end
+
+	post '/piglatinize' do
+		pl = PigLatinizer.new
+		@piglatin = pl.piglatinize(params[:user_phrase])
+
+		#@piglatin
+		erb :results
+	end
+
 end
