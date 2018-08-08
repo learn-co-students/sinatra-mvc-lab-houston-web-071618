31b9a405fd14a16913e6b30f54c7ffd0aba98870
stephen <stephen@farver.io>
2018-08-07 23:20:50 -0500

    Done.

---
+++ models/piglatinizer.rb
@@ -0,0 +1,62 @@
+class PigLatinizer
+#   def piglatinize(sentence)
+#     word_array = sentence.split(' ')
+#
+#     edited_array = word_array.map do |word|
+#       convert_one_word(word)
+#     end
+#
+#     edited_array.join(' ')
+#   end
+#
+#   def convert_one_word(str)
+#     alpha = ('a'..'z').to_a
+#     vowels = %w[a e i o u A E I O U]
+#     consonants = alpha - vowels
+#
+#     if vowels.include?(str[0])
+#       str + 'way'
+#     elsif consonants.include?(str[0]) && consonants.include?(str[1]) && consonants.include?(str[2])
+#       str[3..-1] + str[0..2] + 'ay'
+#     elsif consonants.include?(str[0]) && consonants.include?(str[1])
+#       str[2..-1] + str[0..1] + 'ay'
+#     elsif consonants.include?(str[0])
+#       str[1..-1] + str[0] + 'ay'
+#     else
+#       str # return unchanged
+#     end
+#   end
+#
+  def piglatinize(input_str)
+    x = (input_str.split(" ").length == 1) ? piglatinize_word(input_str) : piglatinize_sentence(input_str)
+    puts x
+    x
+  end
+
+  private
+
+  def consonant?(char)
+    !char.match(/[aAeEiIoOuU]/)
+  end
+
+  def piglatinize_word(word)
+    # word starts with vowel
+    if !consonant?(word[0])
+      word = word + "w"
+    # word starts with 3 consonants
+    elsif consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2])
+      word = word.slice(3..-1) + word.slice(0,3)
+    # word starts with 2 consonants
+    elsif consonant?(word[0]) && consonant?(word[1])
+      word = word.slice(2..-1) + word.slice(0,2)
+    # word starts with 1 consonant
+    else
+      word = word.slice(1..-1) + word.slice(0)
+    end
+    word << "ay"
+  end
+
+  def piglatinize_sentence(sentence)
+    sentence.split.collect { |word| piglatinize_word(word) }.join(" ")
+  end
+end
