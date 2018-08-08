cb91b3bebc57e9ade260b3863d65d8b6fadbb36b
github username <github email address>
2018-08-08 08:47:28 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    erb :user_input
+    # binding.pry
+  end
+
+  post '/piglatinize' do
+    pig = PigLatinizer.new
+    @pl = pig.piglatinize(params[:user_phrase])
+    # binding.pry
+    erb :show
+  end
 end
